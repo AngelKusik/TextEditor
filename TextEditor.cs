1db4611606a99c04539a10afc2fea8c2d959bfee
AngelKusik <angelica.kusik@dcmail.ca>
2022-03-31 19:44:51 -0400

    Missing Open button functionality and review New functionality. I also need to go throught the interface and test all hotkeys and tab indexes.

--- TextEditor.cs
+++ TextEditor.cs
@@ -69,42 +69,76 @@ namespace TextEditor_Lab5
         {
             Close();
         }
+        /// <summary>
+        /// Creates a new document by reseating the textbox to default values.
+        /// </summary>
 
         private void NewClick(object sender, EventArgs e)
         {
-
+            //clear the textbox and reseat the file path variable to create a new document.
+            textBoxBody.Text = String.Empty;
+            openFile = String.Empty;            
         }
 
         private void OpenClick(object sender, EventArgs e)
         {
+            openFile = String.Empty;
 
+           // OpenFileDialog openDialog = new OpenFileDialog();
+            //FileStream fileToAcess = new FileStream(openFile, FileMode.Open, FileAccess.Read);
+           // StreamReader read = new StreamReader(fileToAcess);
+
+            //Read the current file in the textbox into the file.
+            //read.ReadToEnd(fileToAcess.Text.ToString());
+
+            //Close the write 
+            //read.Close();
         }
-
+        /// <summary>
+        /// Cuts a selected section of text from the textbox and pastes it to the clipboard.
+        /// </summary>
         private void CutClick(object sender, EventArgs e)
         {
-
+            //If the user clicks the textBox (the textbox is on focus).
+            if (textBoxBody.Focused)
+            {
+                //Ensure the user selected some text to cut.
+                if (textBoxBody.SelectedText != String.Empty)
+                {
+                    //Cut the selected text in the control and add it to the clipboard using its own method.
+                    CutText(textBoxBody);
+                    //For future reference, I could also have used this: textBoxBody.Cut();
+                }
+                //If the user didn't select any text
+                else
+                {
+                    //Display an error message informing that Cut failed.
+                    MessageBox.Show("Cut Failed.", "No text is selected.");
+                }
+            }
         }
         /// <summary>
         /// Copies all the text or a selected part of the text on the textbox. If there is no text to be copied, issues an error message.
         /// </summary>
         private void CopyClick(object sender, EventArgs e)
         {
-            //If the user clicks the textBox.
+            //If the user clicks the textBox (the textbox is on focus).
             if (textBoxBody.Focused)
             {
                 //If the user selects some text in the textBox 
                 if(textBoxBody.SelectedText != String.Empty)
                 {
-                    //Copy the selected text to the clipboard.
-                    Clipboard.SetText(textBoxBody.SelectedText);
+                    //Copy the selected text to the clipboard using it's own method
+                    CopyText(textBoxBody.SelectedText);
+                    //Here I could also have used the textBoxBody.Copy() function.
                 }
                 //If the user don't select any text in the textBox but the textBox
                 //contains some text.
                 else if(textBoxBody.SelectedText == String.Empty && textBoxBody.Text != String.Empty)
                 {
-                    //Copy all the text in the textBox to the Clipboard and 
+                    //Copy all the text in the textBox to the Clipboard using the CopyText method and 
                     //let the user know everything was copied.
-                    Clipboard.SetText(textBoxBody.Text);
+                    CopyText(textBoxBody.SelectedText);
                     MessageBox.Show("Copy Successful.", "All text in the textbox was copied.");
                 }
                 //If the use attempt to copy but there is no text on the textBox.
@@ -113,22 +147,29 @@ namespace TextEditor_Lab5
                     //Issue an error message.
                     MessageBox.Show("Copy Failed.", "No text is selected.");
                 }
-            //Just in case anything else goes wrong (I heard its a good idea to always have an else on a if block
-            //to handle any exceptions that I may not have antecipated.)
-            }else
-            {
-                MessageBox.Show("Ops.. something went wrong.");
             }
         }
-
+        /// <summary>
+        /// Pastes text from the clipboard over a selected text section of the textbox or after the last
+        /// text character.
+        /// </summary>
         private void PasteClick(object sender, EventArgs e)
         {
-
+            //First check if is there any text in the Clipboard to be pasted in the textbox.
+            if (Clipboard.ContainsText())
+            {
+                //If the Clipboard contains text call the PasteFunction() to paste it on the textbox.
+                PasteText(textBoxBody);
+            }
         }
-
+        /// <summary>
+        /// Dislays information about the application.
+        /// </summary>
         private void AboutClick(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Alf Text Editor" + Environment.NewLine +
+            "By Angelica Kusik in partnership with Professor Kyle Chapman" + Environment.NewLine + 
+            "\u00a9 2022 Angelica Kusik CO. All rights reserved.");
         }
 
         #endregion
@@ -144,6 +185,36 @@ namespace TextEditor_Lab5
 
             //Close the write 
             write.Close();
+        }
+        /// <summary>
+        /// Removes the selected text when the user clicks cut and add it to the clipboard.
+        /// </summary>
+        /// <param name="textbox">Passes the textBoxBody as a parameter</param>
+        private void CutText(TextBox textbox)
+        {
+            //Add the cut text to the clipboard.
+            Clipboard.SetText(textbox.SelectedText);
+
+            //Create a variable to identify the end point of the selected text to be removed. 
+            int selectionEnd = textbox.SelectionLength;
+            //Remove the selected text from the textbox.
+            textbox.Text = textbox.Text.Remove(textbox.SelectionStart, selectionEnd);
+        }
+        /// <summary>
+        /// Copies selected text to the clipboard.
+        /// </summary>
+        /// <param name="selectedText"></param>
+        private void CopyText(string selectedText)
+        {
+            //Copy the selected text to the clipboard.
+            Clipboard.SetText(selectedText);
+        }
+
+        private void PasteText(TextBox textbox)
+        {
+            //Now paste the text from the clipboard where the user selected.
+            textbox.Text = textbox.Text.Insert(textbox.SelectionStart, Clipboard.GetText());
+
         }
         #endregion
     }
