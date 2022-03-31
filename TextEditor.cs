4e943cf123a3730e335df63ff7d46d2bf0dc3056
AngelKusik <angelica.kusik@dcmail.ca>
2022-03-31 13:40:31 -0400

    Save, SaveAs, Exit and Copy functionalities working.

--- TextEditor.cs
+++ TextEditor.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Author: Angelica Kusik
+// Date: March 31, 2022
+// Description: 
+// A simple text editor application created as Lab 5 requirement for the NETD course that provides the user with some
+//basic text edition features such as save, save as, copy, paste, cut, and more.
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,19 +19,24 @@ namespace TextEditor_Lab5
 {
     public partial class formTextEditor : Form
     {
+        //Declare a "global" variable to hold the file path and set it
+        //equal to an empty string until the user selects where the file must be saved.
         string openFile = String.Empty;
         public formTextEditor()
         {
             InitializeComponent();
         }
         #region "Event Handlers"
+        /// <summary>
+        /// Saves the file to the selected location.
+        /// </summary>
         private void SaveClick(object sender, EventArgs e)
         {
             //if openFile (the file that is currently open) has no value,
             //call "Save As" instead!
             if (openFile == String.Empty)
             {
-                //SaveAsClick();
+                SaveAsClick(sender, e);
             }
             //If openFile has a value, just call the SaveFile() function!
             else if (openFile != String.Empty)
@@ -34,20 +45,26 @@ namespace TextEditor_Lab5
             }
         }
         /// <summary>
-        /// TODO: Add description
+        /// Allows the user to select the location where the file must be saved.
         /// </summary>
         private void SaveAsClick(object sender, EventArgs e)
         {
+            //Declare, instantiate and configure a new SaveFileDialog
             SaveFileDialog saveDialog = new SaveFileDialog();
             saveDialog.Filter = "Text Files (*.alf)|*.alf";
 
+            //If the person picks a file and clicks "OK"
             if (saveDialog.ShowDialog() == DialogResult.OK)
             {
+                //Set openFile equal to the File path 
                 openFile = saveDialog.FileName;
-
+                //Call the SaveFile function to write the contents of the textbox into the file.
                 SaveFile(openFile);
             }
         }
+        /// <summary>
+        /// Closes the application.
+        /// </summary>
         private void ExitClick(object sender, EventArgs e)
         {
             Close();
@@ -67,10 +84,41 @@ namespace TextEditor_Lab5
         {
 
         }
-
+        /// <summary>
+        /// Copies all the text or a selected part of the text on the textbox. If there is no text to be copied, issues an error message.
+        /// </summary>
         private void CopyClick(object sender, EventArgs e)
         {
-
+            //If the user clicks the textBox.
+            if (textBoxBody.Focused)
+            {
+                //If the user selects some text in the textBox 
+                if(textBoxBody.SelectedText != String.Empty)
+                {
+                    //Copy the selected text to the clipboard.
+                    Clipboard.SetText(textBoxBody.SelectedText);
+                }
+                //If the user don't select any text in the textBox but the textBox
+                //contains some text.
+                else if(textBoxBody.SelectedText == String.Empty && textBoxBody.Text != String.Empty)
+                {
+                    //Copy all the text in the textBox to the Clipboard and 
+                    //let the user know everything was copied.
+                    Clipboard.SetText(textBoxBody.Text);
+                    MessageBox.Show("Copy Successful.", "All text in the textbox was copied.");
+                }
+                //If the use attempt to copy but there is no text on the textBox.
+                else
+                {
+                    //Issue an error message.
+                    MessageBox.Show("Copy Failed.", "No text is selected.");
+                }
+            //Just in case anything else goes wrong (I heard its a good idea to always have an else on a if block
+            //to handle any exceptions that I may not have antecipated.)
+            }else
+            {
+                MessageBox.Show("Ops.. something went wrong.");
+            }
         }
 
         private void PasteClick(object sender, EventArgs e)
@@ -87,11 +135,14 @@ namespace TextEditor_Lab5
         #region "Functions"
         private void SaveFile(string fileName)
         {
+            //Create a System.IO object to make file access easier
             FileStream fileToAcess = new FileStream(fileName, FileMode.Create, FileAccess.Write);
             StreamWriter write = new StreamWriter(fileToAcess);
 
+            //Write the current text in the textbox into the file.
             write.Write(textBoxBody.Text);
 
+            //Close the write 
             write.Close();
         }
         #endregion
